692f0a964b67e9f309685f5f17a3cca00c63d4aa
n2oblife <zackanit@gmail.com>
2023-06-08 17:16:47 +0200

    building commit

--- build_package.sh
+++ build_package.sh
@@ -5,15 +5,15 @@ result=${result:-/}         # to correct for the case where PWD=/
 folder="${PWD##*/}"         # for use as shell input
 
 if [ $folder == utils_n2oblife ]; then
-    echo Building utils_n2oblife package
-    python3 -m build
-    echo Installing package in env
+    echo "$(tput bold)" Building utils_n2oblife package "$(tput sgr0)"
+    python -m build
+    echo "$(tput bold)" Installing package in env "$(tput sgr0)"
     pip install -e ./
-    echo Pushing to git : https://github.com/n2oblife/utils_n2oblife
+    echo "$(tput bold)" Pushing to git : "$(tput sgr0)" https://github.com/n2oblife/utils_n2oblife
     git add .
     git commit -m "building commit"
     git push
-    echo Package built and pushed 
+    echo "$(tput bold)" Package built and pushed  "$(tput sgr0)"
 else 
     echo Not in utils_n2oblife folder
 fi
