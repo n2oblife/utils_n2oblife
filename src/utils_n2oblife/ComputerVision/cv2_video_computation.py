cac1cdfa7340a9f98921e600006cff06352756ff
n2oblife <zackanit@gmail.com>
2024-05-24 11:13:48 +0200

    video_compute

--- src/utils_n2oblife/ComputerVision/cv2_video_computation.py
+++ src/utils_n2oblife/ComputerVision/cv2_video_computation.py
@@ -1,16 +1,26 @@
 import cv2 
 import numpy as np
 
-def store_video(file_path:str)->list:
+def store_video(file_path: str) -> list:
+    """
+    Store frames from a video file into a list.
+
+    Args:
+        file_path (str): The path to the video file.
+
+    Returns:
+        list: A list containing the frames of the video.
+    """
     # Create a VideoCapture object and read from input file 
     cap = cv2.VideoCapture(file_path)
 
-    # Check if camera opened successfully 
-    if (cap.isOpened()== False): 
-        print("Error opening video file") 
+    # Check if the video file opened successfully 
+    if not cap.isOpened():
+        print("Error opening video file")
+        return []
 
     frames = []
-    # Read until video is completed 
+    # Read until the video is completed 
     while cap.isOpened():
         # Capture frame-by-frame 
         ret, frame = cap.read()
@@ -19,19 +29,26 @@ def store_video(file_path:str)->list:
         else:
             break
     
-    # When everything done, release 
-    # the video capture object 
+    # Release the video capture object 
     cap.release()
     return frames
 
-def show_video(frames:list, title='frames', frame_rate=30)->None:
-    # Read until video is completed 
+def show_video(frames: list, title='frames', frame_rate=30) -> None:
+    """
+    Display video frames in a window.
+
+    Args:
+        frames (list): A list containing the frames of the video.
+        title (str, optional): The title of the window. Defaults to 'frames'.
+        frame_rate (int, optional): The frame rate for displaying the video. Defaults to 30.
+    """
+    # Display each frame 
     for frame in frames:
         # Display the resulting frame 
         cv2.imshow(title, frame)
-        # Press Q on keyboard to exit 
+        # Press 'q' on keyboard to exit 
         if cv2.waitKey(frame_rate) & 0xFF == ord('q'):
             break
 
-    # Closes all the frames 
+    # Close all the frames 
     cv2.destroyAllWindows()
