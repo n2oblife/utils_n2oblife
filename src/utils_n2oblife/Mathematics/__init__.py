9ea600badb53325400fd90c005405ee31a8fb24c
n2oblife <zackanit@gmail.com>
2024-07-03 10:59:31 +0200

    auto init

--- src/utils_n2oblife/Mathematics/__init__.py
+++ src/utils_n2oblife/Mathematics/__init__.py
@@ -1,1 +1,12 @@
-#from . import 
+### AUTO IMPORTS ###
+# This file is automatically import every other file of the folder as a module
+# To add new scripts, create a new file in this directory.
+# only import this file as in your main to get everything at once
+
+
+import os
+for module in os.listdir(os.path.dirname(__file__)):
+    if module == '__init__.py' or module[-3:] != '.py':
+        continue
+    __import__(module[:-3], locals(), globals())
+del module
